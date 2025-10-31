323a96531e1f7e24852468632809c0024d634185
Beowulf22 <manst-berlin@web.de>
2025-10-31 23:22:04 +0100

    batch 3

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from stats import count_words
-from stats import count_characters
+import sys
+from stats import counting
+from stats import sort_desc
+
+def sys_import():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+    return book_path
 
 def get_book_text(filepath):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at "+filepath+"...")
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    #print(book_text)
-    count_words(book_text)
-    count_by_character = count_characters(book_text)
-    print(count_by_character)
+    book_path = sys_import()
+    book_text = get_book_text(book_path)
+    #stats.py
+    count_by_character = counting(book_text)
+    sort_desc(count_by_character)
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,10 @@
-def count_words(book_text):
+def counting(book_text):
+    print("----------- Word Count ----------")
     words = book_text.split()
     count = len(words)
     print(f"Found {count} total words")
 
-def count_characters(book_text):
+    print("--------- Character Count -------")
     character = {}
     full_text = book_text.lower()
     for text_character in full_text:
@@ -12,3 +13,21 @@ def count_characters(book_text):
         else:
             character[text_character] = 1
     return character
+
+def sort_func(chars):
+    return chars["num"]
+
+def sort_desc(characters):
+    sorted_chars = []
+    for character in characters:
+        character_set = {
+            "char": character,
+            "num": characters[character],
+            }
+        sorted_chars.append(character_set)
+    sorted_chars.sort(reverse=True, key=sort_func)
+
+    for sorted_char in sorted_chars:
+        if sorted_char["char"].isalpha():
+            print(f"{sorted_char["char"]}: {sorted_char["num"]}")
+    print("============= END ===============")
